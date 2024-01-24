0ba4ae4c6e834f81fac9d0480f00650002edc91b
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2024-01-24 15:19:41 -0500

    CNBC Data

--- cnbc.py
+++ cnbc.py
@@ -17,14 +17,26 @@ chromeOptions.add_experimental_option("prefs",prefs)
 chromeOptions.add_argument("window-size=1200x600")
 driver = webdriver.Chrome(options=chromeOptions)
 url=f'https://www.cnbc.com/search/?query=google%20financial%20news&qsearchterm=google%20financial%20news'
-driver.get(url)
-time.sleep(3)
 data={}
+# df=pd.read_csv("News_Data.csv")
 df_list=[]
-merged=[]
+driver.get(url)
+
+time.sleep(3)
+last_height = driver.execute_script("return document.body.scrollHeight")
+# print(driver.find_elements("xpath","/div[@class='SearchResult-searchResultContent']/div/a"))
 for i in range(1,1000):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
     headlines=driver.find_element("xpath",f'/html/body/div[4]/div/div[1]/div[3]/div/div/div/div/div/div[2]/div/section/div/div[2]/div/div[2]/div/div/div[2]/div/div/div[2]/div[{i}]/div/div[2]/div[2]/a')
-    data={"headlines":[headlines.text]}
+    driver.get_screenshot_as_file("screenshot.png")
+    try:
+        date=driver.find_element("xpath",f'/html/body/div[4]/div/div[1]/div[3]/div/div/div/div/div/div[2]/div/section/div/div[2]/div/div[2]/div/div/div[2]/div/div/div[2]/div[{i}]/div/div[2]/span/span[2]')
+    except:
+        date=driver.find_element("xpath",f'/html/body/div[4]/div/div[1]/div[3]/div/div/div/div/div/div[2]/div/section/div/div[2]/div/div[2]/div/div/div[2]/div/div/div[2]/div[{i}]/div/div[2]/span/span')
+
+    data={"headlines":[headlines.text],"date":[date.text]}
+
     driver.get(headlines.get_attribute("href"))
     time.sleep(3)
     l=[]
@@ -45,6 +57,7 @@ for i in range(1,1000):
     df_list.append(pd.DataFrame(data=data))
     pd.concat(df_list).to_csv("News_Data.csv")
     driver.get(url)
-    time.sleep(2)
-
+    
+    time.sleep(3)
+    
 
